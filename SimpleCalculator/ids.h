cdfab0144e61e1443763a65b11be6df66890d0f4
Kinaz Rezek <kinaz_rezek@hotmail.com>
2023-01-07 09:20:52 +0100

    adding ribbon new actions elements

--- SimpleCalculator/ids.h
+++ SimpleCalculator/ids.h
@@ -7,47 +7,53 @@
 
 #define IDC_CMD_TAB_START 20000  /* Start tab */ 
 #define IDC_CMD_TAB_START_LabelTitle_RESID 60001
-#define IDC_CMD_TAB_VIEW 20001  /* View tab */ 
+#define IDC_CMD_TAB_VIEW 30000  /* View tab */ 
 #define IDC_CMD_TAB_VIEW_LabelTitle_RESID 60002
-#define cmdFontOnlyGroup 50001  /* FontOnlyGroup */ 
+#define cmdFontOnlyGroup 31000  /* FontOnlyGroup */ 
 #define cmdFontOnlyGroup_LabelTitle_RESID 60003
-#define cmdFontOnly 50010  /* FontOnly */ 
-#define cmdGroupSymbols 2 
-#define cmdGroupSymbols_LabelTitle_RESID 301
-#define cmdButtonPi 3 
-#define cmdButtonPi_LargeImages_RESID 10010
-#define cmdButtonEuler 4 
-#define cmdButtonEuler_LargeImages_RESID 10011
-#define cmdGroupFunctions 5 
-#define cmdGroupFunctions_LabelTitle_RESID 201
-#define cmdButtonSin 6 
-#define cmdButtonSin_LabelTitle_RESID 203
-#define cmdButtonCos 7 
-#define cmdButtonCos_LabelTitle_RESID 205
-#define cmdButtonTan 8 
-#define cmdButtonTan_LabelTitle_RESID 207
-#define cmdButtonLog 9 
-#define cmdButtonLog_LabelTitle_RESID 209
-#define cmdButtonExp 10 
-#define cmdButtonExp_LabelTitle_RESID 211
-#define cmdButtonAsin 11 
-#define cmdButtonAsin_LabelTitle_RESID 213
-#define cmdButtonAcos 12 
-#define cmdButtonAcos_LabelTitle_RESID 215
-#define cmdButtonAtan 13 
-#define cmdButtonAtan_LabelTitle_RESID 217
-#define cmdButtonLn 14 
-#define cmdButtonLn_LabelTitle_RESID 219
-#define cmdGroupObjects 15 
-#define cmdGroupObjects_LabelTitle_RESID 510
-#define cmdButtonFraction 16 
-#define cmdButtonFraction_LargeImages_RESID 10001
-#define cmdButtonPower 17 
-#define cmdButtonPower_LargeImages_RESID 10002
-#define cmdButtonRoot 18 
-#define cmdButtonRoot_LargeImages_RESID 10003
-#define cmdQat 19 
-#define cmdFileMenu 20 
-#define IDC_CMD_EXIT 21 
+#define cmdFontOnly 31100  /* FontOnly */ 
+#define cmdGroupObjects 2 
+#define cmdGroupObjects_LabelTitle_RESID 21000
+#define cmdButtonFraction 3 
+#define cmdButtonFraction_LargeImages_RESID 21100
+#define cmdButtonPower 4 
+#define cmdButtonPower_LargeImages_RESID 21101
+#define cmdButtonRoot 5 
+#define cmdButtonRoot_LargeImages_RESID 21102
+#define cmdGroupSymbols 6 
+#define cmdGroupSymbols_LabelTitle_RESID 22000
+#define cmdButtonPi 7 
+#define cmdButtonPi_LargeImages_RESID 22100
+#define cmdButtonEuler 8 
+#define cmdButtonEuler_LargeImages_RESID 22101
+#define cmdGroupFunctions 9 
+#define cmdGroupFunctions_LabelTitle_RESID 23000
+#define cmdButtonSin 10 
+#define cmdButtonSin_LabelTitle_RESID 23100
+#define cmdButtonCos 11 
+#define cmdButtonCos_LabelTitle_RESID 23101
+#define cmdButtonTan 12 
+#define cmdButtonTan_LabelTitle_RESID 23102
+#define cmdButtonLog 13 
+#define cmdButtonLog_LabelTitle_RESID 23103
+#define cmdButtonExp 14 
+#define cmdButtonExp_LabelTitle_RESID 23104
+#define cmdButtonAsin 15 
+#define cmdButtonAsin_LabelTitle_RESID 23105
+#define cmdButtonAcos 16 
+#define cmdButtonAcos_LabelTitle_RESID 23106
+#define cmdButtonAtan 17 
+#define cmdButtonAtan_LabelTitle_RESID 23107
+#define cmdButtonLn 18 
+#define cmdButtonLn_LabelTitle_RESID 23108
+#define cmdGroupActions 19 
+#define cmdGroupActions_LabelTitle_RESID 24000
+#define cmdButtonCalc 20 
+#define cmdButtonCalc_LabelTitle_RESID 24100
+#define cmdButtonNew 21 
+#define cmdButtonNew_LabelTitle_RESID 24101
+#define cmdQat 22 
+#define cmdFileMenu 23 
+#define IDC_CMD_EXIT 24 
 #define IDC_CMD_EXIT_LabelTitle_RESID 100
 #define IDC_CMD_EXIT_LargeImages_RESID 102
