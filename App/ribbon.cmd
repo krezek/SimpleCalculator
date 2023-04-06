6dc2acbbfc2691b9dc68941579035c2358f3b8f9
Kinaz Rezek <kinaz_rezek@hotmail.com>
2023-04-06 09:32:11 +0200

    organize

--- App/ribbon.cmd
+++ App/ribbon.cmd
@@ -1,8 +1,4 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 uicc ribbon.xml ribbon.bml /header:ids.h /res:ribbonui.rc
 
-move ids.h src\
-move ribbon.bml src\
-move ribbonui.rc src\
-
 pause
