f892c921aca4e2b5e3a5ed954e0ab4c79c696306
Kinaz Rezek <kinaz_rezek@hotmail.com>
2023-04-15 17:08:29 +0200

    implement rgl_0

--- Simplify/src/regel.c
+++ Simplify/src/regel.c
@@ -1,6 +1,6 @@
 #include "platform.h"
 
-#include <items.h>
+#include <items_ext.h>
 #include <regel.h>
 
 void rgl_0(Item** item);
@@ -14,4 +14,31 @@ struct Regels g_regles[REGEL_COUNT] =
 void rgl_0(Item** item)
 {
 	printf("apply: rgl_0\n");
+	assert((*item)->_objectType == OBJ_Number);
+
+	Item* tmp = *item;
+	ItemNumber* i = (ItemNumber*)*item;
+
+	wchar_t* pptr;
+	if ((pptr = wcschr(i->_str->_str, L'.')))
+	{
+		size_t z = wcslen(pptr + 1);
+		String* s1 = String_init();
+		String* s2 = String_init();
+		String_cpy(s1, i->_str->_str);
+		String_delete_c(s1, pptr - i->_str->_str);
+		String_cpy(s2, L"1");
+		while (z--)
+			String_cat(s2, L"0");
+		*item = (Item*)ItemFrac_init((Item*)ItemInteger_init(_wtoi64(s1->_str)),
+			(Item*)ItemInteger_init(_wtoi64(s2->_str)));
+		String_free(s1);
+		String_free(s2);
+	}
+	else
+	{
+		*item = (Item*)ItemInteger_init(_wtoi64(i->_str->_str));
+	}
+
+	ItemTree_free(&tmp);
 }
